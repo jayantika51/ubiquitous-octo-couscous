9f9b0f73b2e1233c0721f42ccb57a9d46c762ba6
Jayantika2010 <79147549+jayantika51@users.noreply.github.com>
2023-01-18 19:01:42 +0530

    Add files via upload

---
+++ untitled12.py
@@ -0,0 +1,9 @@
+from tkinter import*
+root=Tk()
+  
+root.title("project")
+root.geometry("600x500")
+
+label1["List1"] = "Listed_items : "+str(List1)
+
+List1 = ["shoes","snacks","house keys","water bottle","wallet","clothes","bag","phone","earphones"]
